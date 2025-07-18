f6a94d98a9d90d413467b487d04f46f4ea156cb8
Mehedi <mehedinoorkhan16@gmail.com>
2025-07-18 11:17:11 +0600

    Community Impact section is added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import FeaturedFoods from './FeaturedFoods';
+import AnimatedCounter from './AnimatedCounter';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <FeaturedFoods></FeaturedFoods>
+            <AnimatedCounter></AnimatedCounter>
         </div>
     );
 };
